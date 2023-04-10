6e6f33558d99ecc146c9e04a680a88d799316bab
revelation <108826880+revel4tion@users.noreply.github.com>
2023-04-10 14:31:53 +0100

    v.0.2

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 import os
 import time
-from colorama import init, Fore, Style
 from moviepy.editor import VideoFileClip
-import playsound
 import threading
-
+from tqdm import tqdm
+import playsound
+from shutil import get_terminal_size
 
 class VideoToASCII:
     def __init__(self, filename):
         self.filename = filename
         self.clip = VideoFileClip(filename)
         self.chars = np.asarray(list(' .,:irs@9B#&@'))
-        self.width = 80
-        self.height = 60
+        self.width, self.height = get_terminal_size()
         self.fps = self.clip.fps
+        self.ascii_frames = self.render_video()
 
     def convert_frame_to_ascii(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -23,28 +23,36 @@ class VideoToASCII:
         ascii_frame = self.chars[(resized // 25)]
         return '\n'.join([''.join(row) for row in ascii_frame])
 
-    def play(self):
-        init(autoreset=True)
-        for frame in self.clip.iter_frames():
+    def render_video(self):
+        ascii_frames = []
+        for frame in tqdm(self.clip.iter_frames(), desc="Rendering video", unit=" frames "):
             ascii_frame = self.convert_frame_to_ascii(frame)
-            os.system('cls' if os.name == 'nt' else 'clear')
+            ascii_frames.append(ascii_frame)
+        return ascii_frames
+
+    def display_video(self):
+        for ascii_frame in self.ascii_frames:
+            os.system('cls')
             for row in ascii_frame.split('\n'):
                 line = ''
                 for char in row:
-                    line += f"{Fore.GREEN}{char}{Style.RESET_ALL}"
+                    line += char
                 print(line)
-            time.sleep(1 / self.fps)
+            time.sleep(0.65 / self.fps)
+
+    def play(self):
+        self.display_video()
 
 def sound():
-    playsound.playsound("audio921837129837.mp3")
+    playsound.playsound("CLIMVTEMPAUDIO.mp3")
 
 if __name__ == '__main__':
+    os.system("color 2")
     path = input("Enter path to video: ")
     vid = VideoToASCII(path)
     aud = VideoFileClip(path).audio
-    os.chdir(f"C:/Users/{str(os.getlogin())}/Downloads")
-    with open(f"C:/Users/{str(os.getlogin())}/Downloads/audio921837129837.mp3", 'a') as f:
-        aud.write_audiofile("audio921837129837.mp3")
+    with open(f"C:/Users/{str(os.getlogin())}/Downloads/CLIMVTEMPAUDIO.mp3", 'a') as f:
+        aud.write_audiofile("CLIMVTEMPAUDIO.mp3")
     
     t1 = threading.Thread(target=vid.play)
     t2 = threading.Thread(target=sound)
